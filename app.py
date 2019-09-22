34dde12b707702c734edce281fd6a1022ea39598
MRZ <the.truth.in.lies.yes@gmail.com>
2019-09-22 01:20:45 -0400

    La entrada para propietarios esta hecha

--- app.py
+++ app.py
@@ -1,13 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///baseDeDatos/tasks.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/Tienda.db'
 db = SQLAlchemy(app)#crea un cursor a la base de datos
+
+class tbpropietario(db.Model):
+    idpropietario = db.Column(db.Integer,primary_key=True)
+    nombre = db.Column(db.String(50))
+    apellido = db.Column(db.String(50))
+    telefono = db.Column(db.Integer)
+    anio = db.Column(db.Integer)
+    sexo = db.Column(db.String(5))
+
+class tbmascota(db.Model):
+    idmascota = db.Column(db.Integer,primary_key=True)
+    nombre = db.Column(db.String(20))
+    anio = db.Column(db.Integer)
+    sexo = db.Column(db.String(5))
+    idpropietario = db.Column(db.Integer,foreign_key=tbpropietario.idpropietario)
 
 @app.route('/')
 def principal():
-    return render_template('index.html')
+    revisarUs = tbpropietario.query.all()
+    return render_template('index.html',qryPropietarios=revisarUs)
+
+@app.route('/crearprop',methods=['POST'])
+def crearpropietario():
+    prop = tbpropietario(nombre = request.form['nomb'],apellido = request.form['ape'], telefono = request.form['telf'],anio = request.form['anio'], sexo = request.form['sxo'] )
+    db.session.add(prop)#nos permite agregar contenido a la base de datos
+    db.session.commit()#pone fin a la sesion de ingreso de datos
+    return redirect(url_for('principal'))##redirecciona a una url que queramos en este caso home
 
 
 if __name__=='__main__':
